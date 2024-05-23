dad01280c1fe29dd65b54876a6472c4a6a82b82f
TamKatsura <nguyenminhtam200495@gmail.com>
2024-05-23 15:45:04 +0700

    BaiTap

---
+++ src/bai01/QuanLySinhVien.java
@@ -0,0 +1,12 @@
+package bai01;
+
+public class QuanLySinhVien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SinhVien[] sv;
+		GiangVien[] gvA, gvB;
+		
+	}
+
+}
---
+++ src/bai02/qlsv/QuanLySinhVien.java
@@ -0,0 +1,11 @@
+package bai02.qlsv;
+import bai01.GiangVien;
+import bai01.SinhVien;
+public class QuanLySinhVien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SinhVien[] sv;
+		GiangVien[] gvA, gvB;
+	}
+}
