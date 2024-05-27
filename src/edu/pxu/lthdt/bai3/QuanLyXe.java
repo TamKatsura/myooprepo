eb4ce48ca04c5bde17314e32425417be7288a1ae
TamKatsura <nguyenminhtam200495@gmail.com>
2024-05-27 07:27:26 +0700

    BaiTap

--- src/edu/pxu/lthdt/bai3/QuanLyXe.java
+++ src/edu/pxu/lthdt/bai3/QuanLyXe.java
@@ -4,7 +4,7 @@ public class QuanLyXe {
 
 	public static void main(String[] args) {
 		// TODO code application logic here
-		XeOto car = new XeOto("Lambotghini", 2023, 600);
+		XeOto car = new XeOto("BMW", 2023, 600);
 		XeBus bus = new XeBus("VinBus", 2023, 200, 30);
 		System.out.println(car);
 		System.out.println(bus);
